38625f503acba53e41b7c7a7f471024b9b2aec3d
Emil Skoldator <emil.linden@gtg-educ.com>
2022-01-12 11:15:50 +0100

    Added inputs_outputs.csv. Added code to main.ino. Added docstring.py (used for generating docstring).

---
+++ main.ino
@@ -0,0 +1,29 @@
+#include <afstandssensor.h>
+
+AfstandsSensor afstandssensor(13, 12);
+
+void setup(){
+    Serial.begin(9600);
+    Serial.println("[completed] setup finished ");
+
+}
+
+void loop(){
+    //Serial.println(afstandssensor.afstandCM());
+    Serial.println(analogRead(A0));
+    delay(2000);
+}
+
+
+bool line_sensor_1(){
+    if(analogRead(A0) < 100){
+        return true;
+    return false;
+    }
+}
+bool line_sensor_2(){
+    if(analogRead(A1) < 100){
+        return true;
+    return false;
+    }
+}
